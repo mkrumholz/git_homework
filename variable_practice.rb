ed72d128ca611e21708ef9d51bf3efb2c7bceb88
Molly Krumholz <mkkrumholz@gmail.com>
2021-02-21 12:09:20 -0700

    Update values for calories, needs, wants

--- variable_practice.rb
+++ variable_practice.rb
@@ -1,6 +1,6 @@
-bc_ski_needs = ["shovel", "beacon", "probe", "warm_clothes"]
-bc_ski_wants = ["freshies", "bluebird_skies", "good_company"]
+bc_ski_needs = ["shovel", "beacon", "probe", "warm_clothes", "good_company"]
+bc_ski_wants = ["freshies", "bluebird_skies"]
 min_miles_per_day = 3.5
 max_miles_per_day = 50.0
 is_weather_good = true
-calories_consumed = 750
+calories_consumed = 900
