0379197f239eb41ae110d0cde2d32fb45cfd08b4
Molly Krumholz <mkkrumholz@gmail.com>
2021-02-21 12:05:44 -0700

    Add variables to practice file

---
+++ variable_practice.rb
@@ -0,0 +1,6 @@
+bc_ski_needs = ["shovel", "beacon", "probe", "warm_clothes"]
+bc_ski_wants = ["freshies", "bluebird_skies", "good_company"]
+min_miles_per_day = 3.5
+max_miles_per_day = 50.0
+is_weather_good = true
+calories_consumed = 750
